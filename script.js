b99e224f391359a1deb2a464bf1d070d2573e792
Taii <126676872+oliveiratainara948@users.noreply.github.com>
2023-06-23 16:07:41 -0300

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// While
+let tabuada = 7;
+let contador = 0;
+
+while (contador <=10){
+    let resuldado = tabuada * contador;
+    console.log(tabuada + 'x' + contador + '=' + resuldado);
+    contador++; 
+}
